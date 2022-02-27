aacf5a752de4d184a216ca565d15b1ba73f4376f
HizRadit07 <hizkiagenius@gmail.com>
2022-02-27 13:21:11 +0700

    added neuron implementation minus softmax

--- classes/Layer.py
+++ classes/Layer.py
@@ -1,18 +1,25 @@
+import nntplib
 from classes.Neuron import Neuron
 
 
 class Layer:
     """Layer Class"""
     # Properties
-    alogrithm:function = None
+    alogrithm:str = None
     neurons:list[Neuron] = None
     trainable:bool = None
     name:str = None
-
+    layer_bias:float = None
     # Methods
-    def __init__(self,):
+    def __init__(self, algorithm, n_neurons, trainable, name, layer_bias):
         """Class constructor"""
-        pass
+        #create a layer with n neurons, each having [] as initial_weight
+        self.alogrithm = algorithm
+        self.trainable = trainable
+        self.name = name
+        self.layer_bias = layer_bias
+        for i in range (n_neurons):
+            self.neurons.append(Neuron(algorithm,[],layer_bias))
     
     def calculate(self, input:list[float]) -> list[float]:
         """Calculate layer output from the given input"""
--- classes/Neuron.py
+++ classes/Neuron.py
@@ -6,18 +6,33 @@ from algorithm.Sigmoid import sigmoid
 class Neuron:
     """Neuron Class"""
     # Properties
-    algorithm:function = None
+    algorithm:str = None
     weight:list[float] = None
+    bias:float = None
 
     # Methods
-    def __init__(self, algorithm:function, init_weight:list[float], ) -> None:
+    def __init__(self, algorithm:str, init_weight:list[float],bias:float ) -> None:
         """Class constructor"""
-        pass
+        self.algorithm = algorithm
+        self.weight = init_weight
+        self.bias = bias
 
     def calculate(self, input:list[float], ) -> float:
         """Calculate output with the given input"""
-        return None
-    
+        if self.algorithm == "linear":
+            return linear(self.weight,input,self.bias)
+        elif self.algorithm == "relu":
+            return relu(self.weight,input, self.bias)
+        elif self.algorithm == "sigmoid":
+            return sigmoid(self.weight, input, self.bias)
+        else: #algo == softmax
+            #- softmax implementation belum
+            return linear(self.weight,input,self.bias)
+
+    #function to set weight to neuron 
+    def set_weight(self, new_weight:list[float]):
+        self.weight = new_weight
+
     def update(self, ) -> None:
         """Update Neuron's weight"""
         pass
