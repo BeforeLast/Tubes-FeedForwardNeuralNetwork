8f84cc3d550d2bfaaa1c413edfc9265fcc57841a
christopherchandrasaputra <13519074@std.stei.itb.ac.id>
2022-02-28 18:05:24 +0700

    Refactor Fix code styling

--- classes/Layer.py
+++ classes/Layer.py
@@ -1,65 +1,84 @@
-import nntplib
-from typing_extensions import Self
 from classes.Neuron import Neuron
 
 
 class Layer:
     """Layer Class"""
     # Properties
-    alogrithm:str = None
-    neurons:list[Neuron] = None
-    trainable:bool = None
-    name:str = None
-    layer_bias:float = None
+    algorithm: str = None
+    neurons: list[Neuron] = None
+    trainable: bool = None
+    name: str = None
+    layer_bias: float = None
+
     # Methods
-    def __init__(self, algorithm, n_neurons, trainable, name, layer_bias):
+    def __init__(self, algorithm: str, n_neurons: int,
+                 trainable: bool, name: str, layer_bias: float):
         """Class constructor"""
-        #create a layer with n neurons, each having [] as initial_weight (empty array)
-        #akan diasumsi weight setting untuk either setiap neuron or individual neuron akan dilakukan di model
-        self.alogrithm = algorithm
+        # create a layer with n neurons, each having [] as
+        # initial_weight (empty array)
+        # akan diasumsi weight setting untuk either setiap neuron or
+        # individual neuron akan dilakukan di model
+        self.algorithm = algorithm
         self.trainable = trainable
         self.name = name
         self.layer_bias = layer_bias
-        for i in range (n_neurons):
-            self.neurons.append(Neuron(algorithm,[],layer_bias))
-    
-    #getter setter neuron list
+        self.neurons = []
+        for i in range(n_neurons):
+            self.neurons.append(Neuron(algorithm, []))
+
+    # getter setter neuron list
     def getNeuronList(self) -> list[Neuron]:
         return self.neurons
 
     def setNeuronList(self, new_neurons: list[Neuron]):
         self.neurons = new_neurons
 
-    #getter setter individual neurons
-    def getNeuronAtIndex(self,idx) -> Neuron:
+    # getter setter individual neurons
+    def getNeuronAtIndex(self, idx) -> Neuron:
         return self.neurons[idx]
 
-    def setNeuronAtIndex(self, new_neuron:Neuron, idx):
+    def setNeuronAtIndex(self, new_neuron: Neuron, idx):
         self.neurons[idx] = new_neuron
 
-    #methods for neurons weight setting
+    # methods for neurons weight setting
 
-    #set all neuron on neuron list with the same weight
-    def setAllNeuronWeights(self, set_weight:list[float]):
+    # set all neuron on neuron list with the same weight
+    def setAllNeuronWeights(self, set_weight: list[float]):
         for neuron in self.neurons:
             neuron.setWeight(set_weight)
 
-    #set the weight of individual weight at index
-    def setNeuronWeightAtIndex(self, set_weight:list[float],idx):
+    def setNeuronsWeights(self, neuron_weight: list[list[float]]):
+        for i in range(len(self.neurons)):
+            self.neurons[i].setWeight(neuron_weight[i])
+
+    # set the weight of individual weight at index
+    def setNeuronWeightAtIndex(self, set_weight: list[float], idx):
         self.neurons[idx].setWeight(set_weight)
 
-    #calculate given input and return list of values for each neuron
-    def calculate(self, input:list[float]) -> list[float]:
+    # calculate given input and return list of values for each neuron
+    def calculate(self, input: list[float]) -> list[float]:
         """Calculate layer output from the given input"""
-        output = [float]
+        output = [self.layer_bias] if self.layer_bias else []
 
-        for neuron in self.neurons:
-            curNeuronCalc = neuron.calculate(input)
-            output.append(curNeuronCalc)
+        if self.algorithm == "input":
+            output.extend(input)
+        else:
+            for neuron in self.neurons:
+                curNeuronCalc = neuron.calculate(input)
+                output.append(curNeuronCalc)
 
         return output
-    
+
     def update(self,) -> None:
         """Update each neurons"""
         pass
-    
+
+    def __str__(self) -> str:
+        result = "++++++++++++++++++++++++++++++++++++++++++\n"
+        result += f"L-NAME : {self.name}\n"
+        result += f"ALGORITHM : {self.algorithm}\n"
+        result += f"BIAS : {self.layer_bias}\n"
+        result += "++++++++++++++++++++++++++++++++++++++++++\n"
+        for neuron in self.neurons:
+            result += str(neuron)
+        return result
--- classes/Neuron.py
+++ classes/Neuron.py
@@ -3,36 +3,42 @@ from algorithm.Softmax import softmax
 from algorithm.Linear import linear
 from algorithm.Sigmoid import sigmoid
 
+
 class Neuron:
     """Neuron Class"""
     # Properties
-    algorithm:str = None
-    weight:list[float] = None
-    bias:float = None
+    algorithm: str = None
+    weight: list[float] = None
 
     # Methods
-    def __init__(self, algorithm:str, init_weight:list[float],bias:float ) -> None:
+    def __init__(self, algorithm: str, init_weight: list[float], ) -> None:
         """Class constructor"""
         self.algorithm = algorithm
         self.weight = init_weight
-        self.bias = bias
 
-    def calculate(self, input:list[float], ) -> float:
+    def calculate(self, input: list[float], ) -> float:
         """Calculate output with the given input"""
-        if self.algorithm == "linear":
-            return linear(self.weight,input,self.bias)
-        elif self.algorithm == "relu":
-            return relu(self.weight,input, self.bias)
-        elif self.algorithm == "sigmoid":
-            return sigmoid(self.weight, input, self.bias)
-        else: #algo == softmax
-            #- softmax implementation belum
+        if self.algorithm is None:
+            return input
+        elif self.algorithm.lower() == "linear":
+            return linear(self.weight, input)
+        elif self.algorithm.lower() == "relu":
+            return relu(self.weight, input)
+        elif self.algorithm.lower() == "sigmoid":
+            return sigmoid(self.weight, input)
+        elif self.algorithm.lower() == "softmax":
             return softmax(input)
+        else:  # unknown algorithm
+            return input
 
-    #function to set weight to neuron 
-    def setWeight(self, new_weight:list[float]):
+    # function to set weight to neuron
+    def setWeight(self, new_weight: list[float]):
         self.weight = new_weight
 
     def update(self, ) -> None:
         """Update Neuron's weight"""
         pass
+
+    def __str__(self) -> str:
+        result = f"    Neuron: {self.weight}\n"
+        return result
